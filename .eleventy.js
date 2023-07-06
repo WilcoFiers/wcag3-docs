2a6970e5e30f51e6f55064928cc497133cf0a8be
Wilco Fiers <wilco.fiers@deque.com>
2023-07-06 13:37:54 +0200

    Add gh-pages deploy action

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,11 @@
+const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
+
 module.exports = function(eleventyConfig) {
   eleventyConfig.ignores.add('readme.md');
   eleventyConfig.ignores.add('build/**');
+
+  // Make it easy to deploy to gh-pages
+  eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
 
   // Copy `assets/` to `_site/assets`
   eleventyConfig.addPassthroughCopy("assets");
